81ec566666629159bfbdac79827b9357f9f5b187
amangorach <91610050+amangorach@users.noreply.github.com>
2023-07-21 22:17:57 +0530

    Update python.py

--- python.py
+++ python.py
@@ -10,7 +10,7 @@ START_URL = "https://en.wikipedia.org/wiki/List_of_brown_dwarfs"
 page = requests.get(START_URL)
 print(page)
 
-soup = bs(page.text,'html.parser')
+soup = bs(page.text, 'html.parser')
 star_table = soup.find('table')
 temp_list = []
 table_rows = star_table.find_all('tr')
@@ -18,8 +18,11 @@ table_rows = star_table.find_all('tr')
 for tr in table_rows:
     td = tr.find_all('td')
     row = [i.text.rstrip() for i in td]
-    temp_list.append(row)
     
+    # Check if the row has the expected number of columns (10)
+    if len(row) == 10:
+        temp_list.append(row)
+
 star_names = []
 distance = []
 mass = []
@@ -30,8 +33,8 @@ for i in range(1, len(temp_list)):
     distance.append(temp_list[i][5])
     mass.append(temp_list[i][8])
     radius.append(temp_list[i][9])
-    
-df2 = pd.DataFrame(list(zip(star_names,distance,mass,radius)),columns = ['star_names','distance','mass','radius'])
+
+df2 = pd.DataFrame(list(zip(star_names, distance, mass, radius)), columns=['star_names', 'distance', 'mass', 'radius'])
 print(df2)
 
 df2.to_csv('scraped.csv')
